acb81ce00d79ac48e22f919151ed8bc2f9da5520
James Doverspike <menohack@gmail.com>
2014-01-30 18:18:50 -0500

    Refactored cutscenes to use inheritance. Last commit I switched ed's idle sprite to a better one.

--- Cutscene.py
+++ Cutscene.py
@@ -11,21 +11,11 @@ class Cutscene(State.State):
 
     def init(self):
         self.elapsed = 0   #the elapsed time since frame was changed
-        self.FRAMES = []
-        self.FRAMES.append(pygame.image.load("Images/Cutscene1/1small.png").convert())
-        self.FRAMES.append(pygame.image.load("Images/Cutscene1/2.png").convert())
-        self.FRAMES.append(pygame.image.load("Images/Cutscene1/3.png").convert())
-        self.FRAMES.append(pygame.image.load("Images/Cutscene1/4.png").convert())
         self.black = pygame.image.load("Images/AlphaBlack.png").convert_alpha()
         self.image = self.FRAMES[0]
-        self.frametimes = []
-        self.frametimes.append(5)
-        self.frametimes.append(3)
-        self.frametimes.append(2)
-        self.frametimes.append(2)
         self.framecounter = 0
         self.frametime = self.frametimes[0]
-        self.numframes = 4
+        self.numframes = len(self.FRAMES)
         Globals.Elapsed = 0
         Globals.Curtick = pygame.time.get_ticks()
         Globals.Lasttick = 0
@@ -77,73 +67,44 @@ class Cutscene(State.State):
 
 class Cutscene1(Cutscene):
     def __init__(self):
-        self.init()
+        self.FRAMES = []
+        self.FRAMES.append(pygame.image.load("Images/Cutscene1/1small.png").convert())
+        self.FRAMES.append(pygame.image.load("Images/Cutscene1/2.png").convert())
+        self.FRAMES.append(pygame.image.load("Images/Cutscene1/3.png").convert())
+        self.FRAMES.append(pygame.image.load("Images/Cutscene1/4.png").convert())
+        self.frametimes = []
+        self.frametimes.append(5)
+        self.frametimes.append(3)
+        self.frametimes.append(2)
+        self.frametimes.append(2)
+        
         self.nextLevel = Game.Level1()
+        
+        self.init()
+        
 
-class Cutscene2(State.State):
+class Cutscene2(Cutscene):
 
     def __init__(self):
-        self.elapsed = 0   #the elapsed time since frame was changed
         self.FRAMES = []
         self.FRAMES.append(pygame.image.load("Images/Cutscene2/1.png").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene2/2.png").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene2/3.png").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene2/4.png").convert())
-        self.black = pygame.image.load("Images/AlphaBlack.png").convert_alpha()
-        self.image = self.FRAMES[0]
         self.frametimes = []
         self.frametimes.append(5)
         self.frametimes.append(3)
         self.frametimes.append(3)
         self.frametimes.append(2)
-        self.framecounter = 0
-        self.frametime = self.frametimes[0]
-        self.numframes = 4
-        Globals.Elapsed = 0
-        Globals.Curtick = pygame.time.get_ticks()
-        Globals.Lasttick = 0
-        self.fadetime = float(1000)
-        self.backgroundTrack = pygame.mixer.Sound("Sounds/EdwardsCurse.ogg")
-        self.musicplaying = False
-        
 
-
-    def draw(self):
-        Globals.Screen.blit(self.black, (0,0))
-        Globals.Screen.blit(self.image, (0,0))             
-        pygame.display.flip()
+        self.nextLevel = Game.Level2()
         
-    def update(self):
-        Globals.Lasttick = Globals.Curtick
-        Globals.Curtick = pygame.time.get_ticks()
-        self.elapsed += Globals.Curtick - Globals.Lasttick
-        if self.elapsed < 1000:
-            #print int((self.elapsed / self.fadetime) * 255)
-            self.image.set_alpha(int((self.elapsed / self.fadetime) * 255))
-        if not self.musicplaying:
-            self.backgroundTrack.play(-1,0,0)
-            self.backgroundTrack.set_volume(.1)
-            self.musicplaying = True
+        self.init()
         
-
-        if self.elapsed / 1000 > self.frametime:
-            self.elapsed = 0
-            if self.framecounter == self.numframes - 1:
-                Globals.State.backgroundTrack.stop()
-                Globals.State = Game.Level2()
-            else:
-                self.framecounter += 1
-                #print self.framecounter
-                self.image = self.FRAMES[self.framecounter]
-                self.frametime = self.frametimes[self.framecounter]
-class Cutscene3(State.State):
+class Cutscene3(Cutscene):
 
     def __init__(self):
-        self.elapsed = 0   #the elapsed time since frame was changed
         self.FRAMES = []
-        self.numframes = 8
-        #for i in range(1,self.numframes):
-            #self.FRAMES.append(pygame.image.load("Images/Cutscene3/scene3-"+ str(i) + ".png").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene3/scene3-1.png").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene3/scene3-2.png").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene3/scene3-3.png").convert())
@@ -152,8 +113,6 @@ class Cutscene3(State.State):
         self.FRAMES.append(pygame.image.load("Images/Cutscene3/scene3-6.png").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene3/scene3-7.png").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene3/scene3-8.png").convert())
-        self.black = pygame.image.load("Images/AlphaBlack.png").convert_alpha()
-        self.image = self.FRAMES[0]
         self.frametimes = []
         self.frametimes.append(5)
         self.frametimes.append(2)
@@ -163,107 +122,34 @@ class Cutscene3(State.State):
         self.frametimes.append(3)
         self.frametimes.append(2)
         self.frametimes.append(6)
-        self.framecounter = 0
-        self.frametime = self.frametimes[0]
-        Globals.Elapsed = 0
-        Globals.Curtick = pygame.time.get_ticks()
-        Globals.Lasttick = 0
-        self.fadetime = float(1000)
-        self.backgroundTrack = pygame.mixer.Sound("Sounds/EdwardsCurse.ogg")
-        self.musicplaying = False
-        
 
-
-    def draw(self):
-        Globals.Screen.blit(self.black, (0,0))
-        Globals.Screen.blit(self.image, (0,0))             
-        pygame.display.flip()
+        self.nextLevel = Game.Level3()
         
-    def update(self):
-        Globals.Lasttick = Globals.Curtick
-        Globals.Curtick = pygame.time.get_ticks()
-        self.elapsed += Globals.Curtick - Globals.Lasttick
-        if self.elapsed < 1000:
-            #print int((self.elapsed / self.fadetime) * 255)
-            self.image.set_alpha(int((self.elapsed / self.fadetime) * 255))
-        if not self.musicplaying:
-            self.backgroundTrack.play(-1,0,0)
-            self.backgroundTrack.set_volume(.1)
-            self.musicplaying = True
-        
+        self.init()
 
-        if self.elapsed / 1000 > self.frametime:
-            self.elapsed = 0
-            if self.framecounter == self.numframes - 1:
-                Globals.State.backgroundTrack.stop()
-                Globals.State = Game.Level3()
-            else:
-                self.framecounter += 1
-                #print self.framecounter
-                self.image = self.FRAMES[self.framecounter]
-                self.frametime = self.frametimes[self.framecounter]
-class Cutscene4(State.State):
+class Cutscene4(Cutscene):
 
     def __init__(self):
-        self.elapsed = 0   #the elapsed time since frame was changed
         self.FRAMES = []
         self.FRAMES.append(pygame.image.load("Images/Cutscene4/scene4-1.jpg").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene4/scene4-2.png").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene4/scene4-3.png").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene4/scene4-4.png").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene4/scene4-5.png").convert())
-        self.black = pygame.image.load("Images/AlphaBlack.png").convert_alpha()
-        self.image = self.FRAMES[0]
         self.frametimes = []
         self.frametimes.append(5)
         self.frametimes.append(2)
         self.frametimes.append(2)
         self.frametimes.append(3)
         self.frametimes.append(2)
-        self.framecounter = 0
-        self.frametime = self.frametimes[0]
-        self.numframes = 5
-        Globals.Elapsed = 0
-        Globals.Curtick = pygame.time.get_ticks()
-        Globals.Lasttick = 0
-        self.fadetime = float(1000)
-        self.backgroundTrack = pygame.mixer.Sound("Sounds/EdwardsCurse.ogg")
-        self.musicplaying = False
-        
 
-
-    def draw(self):
-        Globals.Screen.blit(self.black, (0,0))
-        Globals.Screen.blit(self.image, (0,0))             
-        pygame.display.flip()
+        self.nextLevel = Game.Level4()
         
-    def update(self):
-        Globals.Lasttick = Globals.Curtick
-        Globals.Curtick = pygame.time.get_ticks()
-        self.elapsed += Globals.Curtick - Globals.Lasttick
-        #if self.elapsed < 1000:
-            #print int((self.elapsed / self.fadetime) * 255)
-            #self.image.set_alpha(int((self.elapsed / self.fadetime) * 255))
-        if not self.musicplaying:
-            self.backgroundTrack.play(-1,0,0)
-            self.backgroundTrack.set_volume(.1)
-            self.musicplaying = True
-        
+        self.init()
 
-        if self.elapsed / 1000 > self.frametime:
-            self.elapsed = 0
-            if self.framecounter == self.numframes - 1:
-                Globals.State.backgroundTrack.stop()
-                Globals.State = Game.Level4()
-            else:
-                self.framecounter += 1
-                #print self.framecounter
-                self.image = self.FRAMES[self.framecounter]
-                self.frametime = self.frametimes[self.framecounter]
-class Cutscene5(State.State):
+class Cutscene5(Cutscene):
 
     def __init__(self):
-        self.elapsed = 0   #the elapsed time since frame was changed
         self.FRAMES = []
         self.FRAMES.append(pygame.image.load("Images/Cutscene5/scene5-1.png").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene5/scene5-2.png").convert())
@@ -273,8 +159,6 @@ class Cutscene5(State.State):
         self.FRAMES.append(pygame.image.load("Images/Cutscene5/scene5-6.png").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene5/scene5-7.png").convert())
         self.FRAMES.append(pygame.image.load("Images/Cutscene5/scene5-8.png").convert())
-        self.black = pygame.image.load("Images/AlphaBlack.png").convert_alpha()
-        self.image = self.FRAMES[0]
         self.frametimes = []
         self.frametimes.append(5)
         self.frametimes.append(2)
@@ -284,45 +168,8 @@ class Cutscene5(State.State):
         self.frametimes.append(2)
         self.frametimes.append(1)
         self.frametimes.append(2)
-        self.framecounter = 0
-        self.frametime = self.frametimes[0]
-        self.numframes = 8
-        Globals.Elapsed = 0
-        Globals.Curtick = pygame.time.get_ticks()
-        Globals.Lasttick = 0
-        self.fadetime = float(1000)
-        self.backgroundTrack = pygame.mixer.Sound("Sounds/EdwardsCurse.ogg")
-        self.musicplaying = False
-        
 
+        self.nextLevel = Victory.Victory()
+        
+        self.init()
 
-    def draw(self):
-        Globals.Screen.blit(self.black, (0,0))
-        Globals.Screen.blit(self.image, (0,0))             
-        pygame.display.flip()
-        
-    def update(self):
-        Globals.Lasttick = Globals.Curtick
-        Globals.Curtick = pygame.time.get_ticks()
-        self.elapsed += Globals.Curtick - Globals.Lasttick
-        if self.elapsed < 1000:
-            #print int((self.elapsed / self.fadetime) * 255)
-            self.image.set_alpha(int((self.elapsed / self.fadetime) * 255))
-        if not self.musicplaying:
-            self.backgroundTrack.play(-1,0,0)
-            self.backgroundTrack.set_volume(.1)
-            self.musicplaying = True
-        
-
-        if self.elapsed / 1000 > self.frametime:
-            self.elapsed = 0
-            if self.framecounter == self.numframes - 1:
-                Globals.State.backgroundTrack.stop()
-                Globals.State = Victory.Victory()
-            else:
-                self.framecounter += 1
-                #print self.framecounter
-                self.image = self.FRAMES[self.framecounter]
-                self.frametime = self.frametimes[self.framecounter]
-            
-        
